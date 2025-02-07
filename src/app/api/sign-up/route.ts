ce4cf739c58b79e712cffc6836edbbe69f5266bd
Satyam Rai <satyamrai553@gmail.com>
2025-02-07 23:49:02 +0530

    add routes

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -4,4 +4,36 @@ import { UserModel } from "@/model/User";
 import bcrypt from 'bcryptjs'
 
 
-export async function POSt
+export async function POST(request: Request){
+    await dbConnect()
+    try {
+      const {username,  email, password, } = await request.json()
+      const existedUserVerifiedByUsername = await UserModel.findOne({
+        username,
+        isVerified: true
+      })
+      if(existedUserVerifiedByUsername){
+        return Response.json(
+            {
+                sucess: false,
+                message: "Username is already taken"
+            },
+            {
+                status: 400
+            }
+        )
+      }
+      
+    } catch (error) {
+        console.error('Error registering user', error)
+        return Response.json(
+            {
+                success: false,
+                message: "Error registering user"
+            },
+            {
+                status: 500
+            }
+        )
+    }
+}
